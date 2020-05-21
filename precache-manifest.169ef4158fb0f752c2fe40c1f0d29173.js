7da62cdfd961d0a85c864e79190d4a7dad2ac90f
Lilian Shaer <Lilian.Shaer@clicktale.com>
2020-05-21 11:23:00 +0300

    Updates

---
+++ precache-manifest.169ef4158fb0f752c2fe40c1f0d29173.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bef88dc05f8a6a7aabc40b6369262fde",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "ebec9b2f5f5eaf0e5dff",
+    "url": "/react-gh-pages/static/css/main.9456322f.chunk.css"
+  },
+  {
+    "revision": "2489a50cc9c73a43aa57",
+    "url": "/react-gh-pages/static/js/2.94590185.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-gh-pages/static/js/2.94590185.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebec9b2f5f5eaf0e5dff",
+    "url": "/react-gh-pages/static/js/main.017998a5.chunk.js"
+  },
+  {
+    "revision": "153f191d5abcb2dc8e12",
+    "url": "/react-gh-pages/static/js/runtime-main.f2a3139b.js"
+  }
+]);
